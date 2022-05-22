c9ae4cebaee4bd0dbffdaa9723cbb67ce1952a90
MonzonJoaquin <Monzonjoaquin@outlook.com>
2022-05-22 16:51:13 -0300

    models-correcion de errores-nombres

--- api/src/db.js
+++ api/src/db.js
@@ -31,7 +31,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 const { Race } = sequelize.models;
-const { Temper } = sequelize.models;
+const { Temperament } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
