cfaca64b8e5bd15e9d5b89dc47cd7066a40ff7ed
MonzonJoaquin <Monzonjoaquin@outlook.com>
2022-05-25 12:43:39 -0300

    correción de rutas y anidamiento de rutas

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -1,25 +1,28 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export default function NavList() {
 	return (
-		<nav>
-			<ul>
-				<li>
-					<NavLink to="/">Home</NavLink>
-				</li>
-				<li>
-					<NavLink to="/main">Listado de perros</NavLink>
-				</li>
-				<li>
-					<NavLink to="/creation">Crear raza de perro</NavLink>
-				</li>
-				<li>
-					<NavLink to="/account">Cuenta</NavLink>
-				</li>
-				<li>
-					<NavLink to="/game">Mascota virtual</NavLink>
-				</li>
-			</ul>
-		</nav>
+		<>
+			<nav>
+				<ul>
+					<li>
+						<NavLink to="/">Home</NavLink>
+					</li>
+					<li>
+						<NavLink to="/main">Listado de perros</NavLink>
+					</li>
+					<li>
+						<NavLink to="/creation">Crear raza de perro</NavLink>
+					</li>
+					<li>
+						<NavLink to="/account">Cuenta</NavLink>
+					</li>
+					<li>
+						<NavLink to="/game">Mascota virtual</NavLink>
+					</li>
+				</ul>
+			</nav>
+			<Outlet />
+		</>
 	);
 }
---
+++ client/src/routes/pages/DetailBreed_page.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function DetailBreed_page() {
+  return (
+    <div>Detalle de la raza de perro</div>
+  )
+}
+
